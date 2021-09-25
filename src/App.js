298553f375012f3a4d172fd1ba54ad5f8f5c1d81
iameffieeee <joanatrinidad12@yahoo.com>
2021-09-25 10:18:02 +0100

    initial commit
    initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="https://i.giphy.com/media/3o7abAHdYvZdBNnGZq/giphy.webp" className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload. ------ Joanaaaa
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
